158ae3f7c11ad18b72acc3a29d5a0f4793c7d9b1
Sidney Júnior <51861308+sidjr2@users.noreply.github.com>
2024-07-02 11:44:35 +0000

    ajuste

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import React, {ReactNode, useState} from "react";
 import IconMenu from "@/components/icons/iconsMenu";
 import IconsAdd from "@/components/icons/iconsAdd";
-import Select from "react-select"; // Importe o componente Select do react-select
+import IconsSelect from "@/components/icons/iconsSelect";
 
 type Props = {
     openSidebarClick  :()=>void;
@@ -16,6 +16,9 @@ export const Header = ({openSidebarClick,title,newChatClick } : Props )=>{
         <header className="flex justify-between items-center w-full border-b border-b-gray-600 p-2 md:hidden">
             <div onClick={openSidebarClick}>
                 <IconMenu width={24} height={24}/>
+            </div>
+            <div>
+                <IconsSelect width={24} height={24}/>
             </div>
             <div className="mx-2 truncate">{title}</div>
             <div onClick={newChatClick}>
